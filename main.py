31e4d165347256e30be3e388f5ac395bd5de0d74
xcaboclean <146889041+xcaboclean@users.noreply.github.com>
2023-10-05 19:51:09 +0000

    Correction of variable type compatibility.

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 print("Welcome to the tip calculator.")
-total_bill = float(input("\nWhat was total bill? $"))
-percentage_tip = int(input("\nWhat perentage tip would you like to give? 10, 12 or 15"))
-number_people = int(input("\nHow many people to split the bill?"))
-bill_per_person = total_bill(1 + (percentage_tip/100))/number_people
 
-print(f"Each person should pay{bill_per_person}.2")
+total_bill = float(input("What was total bill? $"))
+
+percentage_tip = int(input("What percentage tip would you like to give? 10, 12 or 15? "))
+
+percentage_tip_float = percentage_tip/100
+
+number_people = int(input("How many people to split the bill?"))
+
+value_per_person = (total_bill * (1.00 + percentage_tip_float)/ number_people)
+
+value_per_person = round(value_per_person,2)
+print(f"{value_per_person}")
+
+#print(value_per_person)
